e535aa088e3adf41e69cccb02127110c99aec869
Sebastian Thon <sebastian-thon@web.de>
2017-01-31 01:21:12 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace CompactCommand
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            float minutes;
+            float.TryParse(textBox.Text, out minutes);
+            Process.Start("shutdown", "/a");
+            Process.Start("shutdown", "/s /t " + minutes * 60.0f);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("shutdown", "/a");
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
